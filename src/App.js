e7f1deb53b482fb13e165cd1721687ade74b38b2
pedro.jose.memt <pedro.jose@memt.com.br>
2021-05-19 09:22:13 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Comentario from './components/Comentario'
+
+class App extends Component {
+  
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        email: 'joao@mail.com',
+        data: new Date(2020,3,19),
+        mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome: 'Pedro',
+        email: 'pedro@email.com',
+        data: new Date(2020,3,22),
+        mensagem: 'Olá, tudo bem sim...'
+      }
+    ],
+    novoComentario: {
+      nome: '',
+      email: '',
+      mensagem: ''
+    }
+  }
+
+  adicionarcomentario = (evento) => {
+    evento.preventDefault();
+    console.log("Adicionando comentario...")
+    const novoComentario = { ...this.state.novoComentario, data: new Date()}
+    this.setState({
+      comentarios: [...this.state.comentarios, novoComentario],
+      novoComentario: {nome:'', email:'', mensagem:''}
+    })
+  }
+
+  removerComentario = comentario => {
+    let lista = this.state.comentarios;
+    lista = lista.filter(c => c!==comentario)
+    this.setState({comentarios: lista})
+  }
+
+  digitacao = evento => {   
+    const {name, value} = evento.target;
+    this.setState({novoComentario: {...this.state.novoComentario, [name]: value}})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+        {this.state.comentarios.map((comentario, indice) => (
+          <Comentario key={indice} nome={comentario.nome} email={comentario.email} data={comentario.data} onRemove = {this.removerComentario.bind(this, comentario)}>
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+
+        <form method="post" onSubmit={this.adicionarcomentario}>
+          <h2>Adiconar comentario</h2>
+          <div>
+            <input 
+              type="text" 
+              name="nome" 
+              value={this.state.novoComentario.nome} 
+              onChange = {this.digitacao}
+              placeholder="Digite seu nome" />
+          </div>
+          <div>
+            <input type="email" name="email" value={this.state.novoComentario.email} onChange={this.digitacao} placeholder="Digite seu email" />
+          </div>
+          <div>
+            <textarea name="mensagem" rows="4" onChange={this.digitacao} value={this.state.novoComentario.mensagem} />
+          </div>
+          <button type="submit">Adicionar comentário</button>
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Comentario.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Comentario.css';
+
+//JSX
+const Comentario = props => (
+    <div className="Comentario">
+        <h2>{props.nome}:</h2>
+        <p>{props.email}</p>
+        <p>{props.children}</p>
+        <p>{props.data.toString()}</p>
+        <button onClick={props.onRemove}>&times;</button>
+    </div>
+);
+
+export default Comentario;
